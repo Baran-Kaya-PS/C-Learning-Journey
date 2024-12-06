9850f068eccbcf9214afb5095269c46316f23a30
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-06 19:20:10 +0100

    *max(*a,*b) testing

--- main.c
+++ main.c
@@ -15,9 +15,14 @@ int main() {
     long l; // définir le long qui contiendra int(M_PI)
     double d; // définir le double qui contiendra (double) (l - M_PI)
     decompose(M_PI,&l,&d);
-    printf("décomposition de pi %lf = %ld + %lf",M_PI,l,d);
+    printf("decomposition de pi %lf = %ld + %lf\n",M_PI,l,d);
     ///////////////////////////////////////////////////////////////////////////////////
 
     ///////////////////////////////////////////////////////////////////////////////////
+    int *p,i,j;
+    i = 3;
+    j = 4;
+    p = max(&i,&j);
+    printf("value of max(3,4) : %d\n",*p);
     return 0;
 }
