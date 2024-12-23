c670aa6e23730415505daee741e9cb10e20c9fb1
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-23 16:06:28 +0100

    removed : EOF Macro, initially included in stdio.h

--- justify.h
+++ justify.h
@@ -4,8 +4,11 @@
 /*
  * justify file format a quote to justify it
  */
+#include "stdlib.h"
+#include "stdio.h"
 #include "stdbool.h"
+#include "string.h"
 #ifndef MEMORY_GESTION_JUSTIFY_H
 #define MEMORY_GESTION_JUSTIFY_H
 #endif //MEMORY_GESTION_JUSTIFY_H
-#define EOF = '\0'
+/* #define EOF = '\0' */ // replaced by EOF Macro inside stdio.h
