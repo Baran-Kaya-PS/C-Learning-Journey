b6d3b644e0228d9bcdc71257d03b1edd60dfaa33
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-23 16:01:58 +0100

    comment : purpose of justify

--- justify.h
+++ justify.h
@@ -1,8 +1,10 @@
 //
 // Created by Baran on 12/23/2024.
 //
-
+/*
+ * justify file format a quote to justify it
+ */
 #ifndef MEMORY_GESTION_JUSTIFY_H
 #define MEMORY_GESTION_JUSTIFY_H
-
 #endif //MEMORY_GESTION_JUSTIFY_H
+#define EOF = '\0'
