a24eadb2969d997ad135ecee95db84c6ce861896
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-24 13:34:28 +0100

    feat : write_line implementation

--- chapter_15_files_related/line.c
+++ chapter_15_files_related/line.c
@@ -29,3 +29,21 @@ void addword(const char *word){
 int space_remaning(){
     return MAX_LINE_LEN - line_len;
 }
+
+void write_line(void){
+    int extra_spaces, spaces_to_insert,i,j;
+    extra_spaces = space_remaning();
+
+    for (i = 0; i < line_len; i++){
+        if (line[i] != ' ')
+            putchar(line[i]);
+        else {
+            spaces_to_insert = extra_spaces / (num_words-1);
+            for (j = 1; j <= spaces_to_insert+1; j++)
+                putchar(' ');
+            extra_spaces -= spaces_to_insert;
+            num_words--;
+        }
+    }
+    putchar('\n');
+}
