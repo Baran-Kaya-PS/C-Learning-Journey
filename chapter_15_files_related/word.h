c57791c37d3cdc3ad0765dde7443df9f9101c3a8
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-24 12:45:42 +0100

    feat : read_char function definition in the header file

--- chapter_15_files_related/word.h
+++ chapter_15_files_related/word.h
@@ -4,7 +4,7 @@
 
 #ifndef MEMORY_GESTION_WORD_H
 #define MEMORY_GESTION_WORD_H
-
+#include "stdio.h"
 #endif //MEMORY_GESTION_WORD_H
 /*************************************************************************
  * read_word: Reads the next words from the input and stores it in word. *
@@ -12,3 +12,8 @@
  * Truncate the words if its lenght exceeds len.                         *
  *************************************************************************/
 void read_word(char *word, int len);
+
+/*
+ * read_char
+ */
+void read_char(void);
