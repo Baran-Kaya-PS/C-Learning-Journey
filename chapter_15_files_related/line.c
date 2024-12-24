11ad5ce7d6e847d6d6ca6ab7973bda165391cfcb
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-24 13:23:05 +0100

    feat : clearline implementation

--- chapter_15_files_related/line.c
+++ chapter_15_files_related/line.c
@@ -1,5 +1,17 @@
 //
 // Created by Baran on 12/23/2024.
 //
-
+#include <stdio.h>
+#include <string.h>
 #include "line.h"
+
+#define MAX_LINE_LEN 60
+int line_len = 0;
+int num_words = 0;
+char line[MAX_LINE_LEN+1];
+
+void clearline(){
+    line[0] = '\0';
+    line_len = 0;
+    num_words = 0;
+}
