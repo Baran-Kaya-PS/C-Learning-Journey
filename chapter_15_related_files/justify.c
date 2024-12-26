4d890d1856a154d8f7770c02e246f3b442f6d184
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-26 21:22:44 +0100

    moving files of the chapter 15

--- chapter_15_related_files/justify.c
+++ chapter_15_related_files/justify.c
@@ -2,25 +2,24 @@
 // Created by Baran on 12/23/2024.
 //
 #include "justify.h"
-char *justify(){
-    char word[MAX_WORD_LEN+2];
+char *justify() {
+    char word[MAX_WORD_LEN + 2];
     int word_len;
-
     clearline();
-    for (;;){
-        read_word(word,MAX_WORD_LEN+1);
+    for (;;) {
+        read_word(word, MAX_WORD_LEN + 1);
         word_len = strlen(word);
         if (word_len == 0) {
             flush_line();
             return 0;
         }
-        if (word_len > MAX_WORD_LEN){
+        if (word_len > MAX_WORD_LEN) {
             word[MAX_WORD_LEN] = '*';
         }
-        if (word_len+1 > space_remaning()){
+        if (word_len + 1 > space_remaning()) {
             write_line();
             clearline();
         }
         addword(word);
     }
-
+}
